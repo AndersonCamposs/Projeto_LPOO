dc5fb1406730bf3da4298dc0b608844a177d45d0
Anderson Campos <anderson.camposs87@outlook.com>
2024-12-11 20:15:47 -0300

    Criação do painel que contém a JTable de usuários
    Exibição dos dados dos usuários na tabela.
    Adicionando a dependência do Lombok para reduzir o código boiler plate.

--- src/main/java/model/entity/Usuario.java
+++ src/main/java/model/entity/Usuario.java
@@ -8,7 +8,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "Usuario")
 public class Usuario {
@@ -18,37 +22,4 @@ public class Usuario {
     private String nome;
     private String login;
     private String senha;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-    
 }
